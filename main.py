404200a8ce5a7cde1c839d71309698a3dd322664
Keenan <keenan@oatmeal.monster>
2024-08-19 09:44:40 -0700

    add seeds

--- main.py
+++ main.py
@@ -24,13 +24,15 @@ loaders = {"similar": loadSimilar, "random": loadRandom}
 maxes = {
     "playcount": lambda t: 100,  # TODO: This should fetch, or be configured
     "rating": lambda t: 10,
-    "lastplay": lambda t: time.time(),
+    "lastplay": lambda t: time.time(),  # TODO: fix scale here; minimum isn't 0
+    "random": lambda t: 1,
 }
 
 getters = {
     "playcount": lambda t: t.viewCount,
     "rating": lambda t: t.userRating,
     "lastplay": lambda t: t.lastViewedAt.timestamp(),
+    "random": lambda t: random.random(),
 }
 
 
